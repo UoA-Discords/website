12dd1091d5b96cb0d29a7650b416320f13a9ac4f
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-08-18 21:07:33 +1200

    Add header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import { Paper } from '@mui/material';
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <Paper id="header" elevation={0} square>
+            header
+        </Paper>
+    );
+};
+
+export default Header;
