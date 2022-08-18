1ec0a81875dd98c5933604bcad6db9e8be030615
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-08-18 21:12:11 +1200

    update tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test(`renders learn react link`, () => {
+test(`renders the home page`, () => {
     const { getByText } = render(<App />);
 
     // eslint-disable-next-line testing-library/prefer-screen-queries
-    expect(getByText(/learn/i)).toBeInTheDocument();
+    expect(getByText(/uoa discords/i)).toBeInTheDocument();
 });
