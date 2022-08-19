a15d98e633473018b528367000a73a38378f2805
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-08-19 21:28:39 +1200

    test file cleanup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable testing-library/prefer-screen-queries */
-import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
@@ -7,5 +6,4 @@ test(`renders the home page`, () => {
     const { getAllByText } = render(<App />);
 
     expect(getAllByText(/uoa discords/i).length > 0).toBe(true);
-    // expect(getByText(/discords/i)).toBeInTheDocument();
 });
