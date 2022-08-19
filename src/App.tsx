f085ef9eb45290eec3bbb2ec51f483ccd83e28f5
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-08-20 01:10:41 +1200

    add login route to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import './App.css';
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import { Container, Typography } from '@mui/material';
 import AboutPage from './components/AboutPage';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
+import HandleLoginPage from './components/HandleLoginPage';
+import NotFound from './components/NotFound';
+
+import './App.css';
+import './Logo.css';
 
 const App = () => {
     return (
@@ -31,6 +34,15 @@ const App = () => {
                         </Container>
                     }
                 />
+                <Route
+                    path="login"
+                    element={
+                        <Container maxWidth="xl" id="app">
+                            <HandleLoginPage />
+                        </Container>
+                    }
+                />
+                <Route path="*" element={<NotFound />} />
             </Routes>
             <Footer />
         </BrowserRouter>
