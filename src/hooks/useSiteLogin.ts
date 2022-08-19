ec88724084bcf60f385aa5b60906c4923c883f9f
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-08-20 01:09:29 +1200

    implemented site login custom hook

---
+++ src/hooks/useSiteLogin.ts
@@ -0,0 +1,30 @@
+import { RESTPostOAuth2AccessTokenResult } from 'discord-api-types/v10';
+import { useCallback } from 'react';
+import { useCookies } from 'react-cookie';
+import { SiteUser } from '../shared/Types/User';
+
+export interface LoginResponse {
+    discordAuth: RESTPostOAuth2AccessTokenResult;
+    siteAuth: string;
+    userData: SiteUser;
+}
+
+export function useSiteLogin() {
+    const [{ loginResponse }, setLoginResponse, removeLoginResponse] = useCookies<
+        `loginResponse`,
+        { loginResponse?: LoginResponse }
+    >([`loginResponse`]);
+
+    const setUserData = useCallback(
+        (payload: LoginResponse) => {
+            setLoginResponse(`loginResponse`, payload, { sameSite: `strict` });
+        },
+        [setLoginResponse],
+    );
+
+    const clearUserData = useCallback(() => {
+        removeLoginResponse(`loginResponse`);
+    }, [removeLoginResponse]);
+
+    return { loginResponse, setUserData, clearUserData };
+}
