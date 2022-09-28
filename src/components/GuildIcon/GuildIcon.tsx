76ba0d17cdc0fff4a8d713f482e25562e0a96e44
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-09-29 05:19:00 +1300

    more customizable props for `GuildIcon`

--- src/components/GuildIcon/GuildIcon.tsx
+++ src/components/GuildIcon/GuildIcon.tsx
@@ -1,9 +1,15 @@
 import { useState } from 'react';
 import tempLogo from '../../images/tempLogo.png';
 import { EntryStates, FullEntry } from '../../shared/Types/Entries';
-import { CardMedia } from '@mui/material';
+import { CardMedia, SxProps, Theme } from '@mui/material';
 
-const GuildIcon = ({ server }: { server: FullEntry<EntryStates.Approved | EntryStates.Featured> }) => {
+const GuildIcon = ({
+    server,
+    sx,
+}: {
+    server: FullEntry<EntryStates.Approved | EntryStates.Featured>;
+    sx?: SxProps<Theme>;
+}) => {
     const [errored, setErrored] = useState(false);
 
     if (!errored) {
@@ -11,13 +17,13 @@ const GuildIcon = ({ server }: { server: FullEntry<EntryStates.Approved | EntryS
             <CardMedia
                 component="img"
                 image={`https://cdn.discordapp.com/icons/${server.id}/${server.guildData.icon}`}
-                sx={{ width: 128, p: 1, borderRadius: `50%` }}
+                sx={{ width: 128, p: 1, borderRadius: `50%`, ...sx }}
                 onError={() => setErrored(true)}
             />
         );
     }
 
-    return <CardMedia component="img" image={tempLogo} sx={{ width: 128, p: 2 }} />;
+    return <CardMedia component="img" image={tempLogo} sx={{ width: 128, p: 2, ...sx }} />;
 };
 
 export default GuildIcon;
