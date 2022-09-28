38ae2bb9168f15b37bd32f60dbb490c49889ad66
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-09-28 19:47:55 +1300

    `ProfilePicture` component tweaks

--- src/components/ProfilePicture/ProfilePicture.tsx
+++ src/components/ProfilePicture/ProfilePicture.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BasicUserInfo } from '../../shared/Types/User';
 import discordIcon from '../../images/discordIcon.svg';
 
 const ProfilePicture = ({ user, width, height }: { user: BasicUserInfo; width?: number; height?: number }) => {
-    if (user.avatar !== null) {
+    const [avatar, setAvatar] = useState(user.avatar);
+
+    if (avatar !== null) {
         return (
             <img
                 width={width ?? 64}
                 height={height ?? 64}
                 src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`}
                 alt={`${user.username}'s Discord profile`}
-                className="discordProfilePicture"
+                style={{ borderRadius: `50%` }}
+                onError={() => setAvatar(null)}
             />
         );
     }
@@ -19,10 +22,9 @@ const ProfilePicture = ({ user, width, height }: { user: BasicUserInfo; width?: 
         <img
             width={width ?? 64}
             height={height ?? 64}
-            style={{ padding: `0.5em` }}
+            style={{ padding: `0.2em` }}
             src={discordIcon}
             alt="Discord logo"
-            className="discordProfilePicture"
         />
     );
 };
