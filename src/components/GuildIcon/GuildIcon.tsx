6492486cf3d1ea779c9e172cc8f01d29506e144e
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-09-28 19:47:21 +1300

    added `GuildIcon` component

---
+++ src/components/GuildIcon/GuildIcon.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import tempLogo from '../../images/tempLogo.png';
+import { EntryStates, FullEntry } from '../../shared/Types/Entries';
+import { CardMedia } from '@mui/material';
+
+const GuildIcon = ({ server }: { server: FullEntry<EntryStates.Approved | EntryStates.Featured> }) => {
+    const [errored, setErrored] = useState(false);
+
+    if (!errored) {
+        return (
+            <CardMedia
+                component="img"
+                image={`https://cdn.discordapp.com/icons/${server.id}/${server.guildData.icon}`}
+                sx={{ width: 128, p: 1 }}
+                className="discordProfilePicture"
+                onError={() => setErrored(true)}
+            />
+        );
+    }
+
+    return <CardMedia component="img" image={tempLogo} sx={{ width: 128, p: 2 }} />;
+};
+
+export default GuildIcon;
