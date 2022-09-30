31cd59b64c210a7f31c808c69c9375ffe4eb8ed1
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-09-30 20:53:39 +1300

    featured entry sorting above rest of servers

--- src/components/Pages/Home/HomePage.tsx
+++ src/components/Pages/Home/HomePage.tsx
@@ -2,6 +2,7 @@ import { Container, Grid, LinearProgress, Typography } from '@mui/material';
 import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { getAllEntries, getDoneInitialLoad, getVisibleEntries } from '../../../redux/slices/entryManager';
+import { EntryStates } from '../../../shared/Types/Entries';
 import SearchBar from '../../SearchBar';
 import ServerCard from './ServerCard';
 
@@ -11,7 +12,19 @@ const HomePage = () => {
     const doneInitialLoad = useSelector(getDoneInitialLoad);
 
     const sortedEntries = useMemo(
-        () => entryKeys.map((e) => allEntries[e]).sort((a, b) => b.likes - a.likes),
+        () =>
+            entryKeys
+                .map((e) => allEntries[e])
+                .sort((a, b) => {
+                    if (a.state === EntryStates.Featured && b.state !== EntryStates.Featured) {
+                        return -1;
+                    }
+                    if (a.state !== EntryStates.Featured && b.state === EntryStates.Featured) {
+                        return 1;
+                    }
+
+                    return b.likes - a.likes;
+                }),
         [allEntries, entryKeys],
     );
 
@@ -21,13 +34,11 @@ const HomePage = () => {
             <Container id="app" sx={{ mt: 3 }}>
                 <Grid container spacing={2}>
                     {sortedEntries.length ? (
-                        sortedEntries.map((e, i) => {
-                            return (
-                                <Grid item key={e.id} xs={12} md={6} lg={4}>
-                                    <ServerCard server={e} index={i} />
-                                </Grid>
-                            );
-                        })
+                        sortedEntries.map((e, i) => (
+                            <Grid item key={e.id} xs={12} md={6} lg={4}>
+                                <ServerCard server={e} index={i} />
+                            </Grid>
+                        ))
                     ) : (
                         <Grid item xs={12}>
                             {doneInitialLoad ? (
