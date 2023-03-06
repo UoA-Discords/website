6129b239b1050e9a7c0ddc10a61f87f3672ce6ca
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-03-06 22:01:13 +1300

    update index rendering

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import ReactDOM from 'react-dom/client';
+import './index.css';
+import { App } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
-root.render(<h1>hello world {process.env.REACT_APP_VERSION}</h1>);
+root.render(<App />);
