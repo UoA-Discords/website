8a80b39699ec980b3e81ce35a2b5098f125f9b7f
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-03-06 22:00:59 +1300

    add theme

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { createTheme, responsiveFontSizes } from '@mui/material';
+
+export const theme = responsiveFontSizes(
+    createTheme({
+        palette: {
+            mode: 'dark',
+        },
+    }),
+);
