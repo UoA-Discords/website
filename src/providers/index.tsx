0c81ba82830aaaf971821e0076897c84bd53b6e4
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-03-11 01:45:15 +1300

    update content provider syntax

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,11 +1,11 @@
-import React, { ReactNode } from 'react';
+import { FC, ReactNode } from 'react';
 import { LocationDataProvider } from './LocationData/LocationDataProvider';
 import { MainStateProvider } from './MainState/MainStateProvider';
 import { SettingsContextProvider } from './Settings/SettingsProvider';
 import { UserDictionaryProvider } from './UserDictionary/UserDictionaryProvider';
 import { UserSessionContextProvider } from './UserSession/UserSessionProvider';
 
-export const ContextProviders: React.FC<{ children: ReactNode }> = ({ children }) => (
+export const ContextProviders: FC<{ children: ReactNode }> = ({ children }) => (
     <SettingsContextProvider>
         <MainStateProvider>
             <UserSessionContextProvider>
