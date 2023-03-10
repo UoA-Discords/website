8bd697266ce310e9702943bd7fd3758908ea1b51
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-03-11 01:43:04 +1300

    update relative time string syntax

--- src/components/RelativeTimeString/RelativeTimeString.tsx
+++ src/components/RelativeTimeString/RelativeTimeString.tsx
@@ -1,28 +1,33 @@
-import React, { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import { CSSProperties, FC, useEffect, useState } from 'react';
 
 dayjs.extend(relativeTime);
 
-export interface RelativeTimeStringProps extends React.CSSProperties {
+export interface RelativeTimeStringProps extends CSSProperties {
     /** The ISO string or timestamp of the time to display relative to now. */
     time: string | number;
+
     /** If true, displays relative time without suffix, e.g. "2 days" instead of "2 days ago". */
     withoutSuffix?: boolean;
+
     /** Interval between re-renders in milliseconds, default 1000. */
     intervalMs?: number;
+
     /** Whether to wrap the relative time in brackets. */
     inBrackets?: boolean;
 }
 
 /** Displays relative time parsed using {@link https://day.js.org/ dayjs}, updating it every X milliseconds. */
-export const RelativeTimeString: React.FC<RelativeTimeStringProps> = (props) => {
-    const { time, withoutSuffix, intervalMs, inBrackets, ...style } = props;
+export const RelativeTimeString: FC<RelativeTimeStringProps> = (props) => {
+    const { time, withoutSuffix, intervalMs = 1_000, inBrackets, ...style } = props;
 
     const [relativeTime, setRelativeTime] = useState(dayjs(time).fromNow(withoutSuffix));
 
     useEffect(() => {
-        const interval = setInterval(() => setRelativeTime(dayjs(time).fromNow(withoutSuffix)), intervalMs ?? 1000);
+        // updating the time every {intervalMs}
+
+        const interval = setInterval(() => setRelativeTime(dayjs(time).fromNow(withoutSuffix)), intervalMs);
 
         return () => {
             clearInterval(interval);
