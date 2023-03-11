3a26e9bdcaaace895b40fefe2222700a09756d16
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-03-12 02:31:24 +1300

    add server list to home page

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,13 +1,72 @@
-import { Typography } from '@mui/material';
-import { FC, useContext, useEffect } from 'react';
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import SearchIcon from '@mui/icons-material/Search';
+import { Collapse, Grid, IconButton, Pagination, Stack, TextField, Typography } from '@mui/material';
+import { FC, useContext, useEffect, useMemo, useState } from 'react';
 import { Page } from '../../Page.styled';
-import { MainStateContext } from '../../contexts';
+import { api } from '../../api';
+import { ServerCard, ServerCardSkeleton } from '../../components/ServerCard';
+import { MainStateContext, SettingsContext, UserSessionContext } from '../../contexts';
 import { LocationDataContext } from '../../contexts/LocationData';
+import { WithPagination } from '../../types/Page';
+import { GetAllServersParams } from '../../types/Searching/GetAllServersParams';
+import { Server } from '../../types/Server';
+import { ServerStatus } from '../../types/Server/ServerStatus';
+import { TagFilterer } from './TagFilterer';
 
 export const HomePage: FC = () => {
-    const { latestServerResponse } = useContext(MainStateContext);
+    const { latestServerResponse, setLatestError } = useContext(MainStateContext);
     const { setLocationData } = useContext(LocationDataContext);
+    const { settings } = useContext(SettingsContext);
+    const { loggedInUser } = useContext(UserSessionContext);
 
+    const [isFetching, setIsFetching] = useState(false);
+    const [searchParams, setSearchParams] = useState<GetAllServersParams>({
+        page: 0,
+        perPage: 20,
+        withStatus: 'visible',
+    });
+    const [serverList, setServerList] = useState<WithPagination<Server>>();
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filterExpanded, setFilterExpanded] = useState(false);
+
+    const sortedServerList = useMemo(() => {
+        if (serverList === undefined) return [];
+
+        if (searchParams.searchTerm !== undefined) return serverList.items;
+
+        if (searchParams.withTags !== undefined) return serverList.items;
+
+        return serverList.items.sort((a, b) => {
+            if (a.status === ServerStatus.Featured && b.status !== ServerStatus.Featured) {
+                return -1;
+            }
+            if (a.status !== ServerStatus.Featured && b.status === ServerStatus.Featured) {
+                return 1;
+            }
+
+            return a._id.localeCompare(b._id);
+        });
+    }, [searchParams.searchTerm, searchParams.withTags, serverList]);
+    useEffect(() => {
+        const controller = new AbortController();
+
+        setIsFetching(true);
+
+        api.searchServers(
+            {
+                baseURL: settings.serverUrl,
+                siteToken: loggedInUser?.siteAuth,
+                controller,
+                rateLimitBypassToken: settings.rateLimitBypassToken,
+            },
+            searchParams,
+        )
+            .then(setServerList)
+            .catch(setLatestError)
+            .finally(() => setIsFetching(false));
+    }, [loggedInUser?.siteAuth, searchParams, setLatestError, settings.rateLimitBypassToken, settings.serverUrl]);
+
+    // updating header text
     useEffect(() => {
         if (latestServerResponse) {
             const { numServers, numUsers } = latestServerResponse;
@@ -22,17 +81,94 @@ export const HomePage: FC = () => {
         }
     }, [latestServerResponse, setLocationData]);
 
+    useEffect(() => {
+        if (searchTerm === '' && searchParams.searchTerm !== undefined) {
+            delete searchParams.searchTerm;
+            setSearchParams({ ...searchParams });
+        }
+    }, [searchParams, searchTerm]);
+
     return (
-        <Page>
-            <Typography variant="h1" textAlign="center">
-                UoA Discords
-            </Typography>
-            <Typography variant="subtitle2" color="gray" sx={{ pl: 1, pr: 1, mb: 3 }} textAlign="center">
-                Your catalogue for the University of Auckland's various Discord servers.
-            </Typography>
-            <Typography color="gold">
-                Hey there! We're currently undergoing some changes right now, so check back later 👻
-            </Typography>
+        <Page maxWidth="xl">
+            <Stack gap={2} alignItems="center" sx={{ mt: 1 }}>
+                <Stack direction="row" gap={3} sx={{ width: '100%' }} justifyContent="space-between" flexWrap="wrap">
+                    <Stack direction="row" alignItems="flex-end" gap={1}>
+                        <TextField
+                            variant="standard"
+                            size="medium"
+                            placeholder="Search servers"
+                            sx={{ minWidth: '250px' }}
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                            InputProps={{
+                                endAdornment: (
+                                    <IconButton onClick={() => setSearchParams({ ...searchParams, searchTerm })}>
+                                        <SearchIcon htmlColor="gray" />
+                                    </IconButton>
+                                ),
+                            }}
+                        />
+
+                        <IconButton
+                            title={searchParams.withTags !== undefined ? 'Tag filtering active' : 'Filter by tags'}
+                            onClick={() => setFilterExpanded(!filterExpanded)}
+                        >
+                            <FilterAltIcon color={searchParams.withTags !== undefined ? 'secondary' : 'disabled'} />
+                        </IconButton>
+                    </Stack>
+
+                    <Pagination
+                        variant="outlined"
+                        shape="rounded"
+                        size="large"
+                        count={Math.ceil((serverList?.totalItemCount ?? 0) / searchParams.perPage)}
+                        page={searchParams.page + 1}
+                        onChange={(_e, page) => setSearchParams({ ...searchParams, page: page - 1 })}
+                    />
+                </Stack>
+
+                <Collapse in={filterExpanded}>
+                    <TagFilterer
+                        onApply={(searchInfo) => {
+                            setSearchParams({ ...searchParams, withTags: searchInfo });
+                        }}
+                        onUnapply={() => {
+                            delete searchParams.withTags;
+                            setSearchParams({ ...searchParams });
+                        }}
+                    />
+                </Collapse>
+
+                <Grid container spacing={2}>
+                    {isFetching
+                        ? new Array(searchParams.perPage).fill(0).map((_e, i) => (
+                              <Grid item key={i} xs={12} md={6} lg={4} sx={{ zIndex: 1 }}>
+                                  <ServerCardSkeleton />
+                              </Grid>
+                          ))
+                        : sortedServerList.map((e, i) => (
+                              <Grid item key={i} xs={12} md={6} lg={4} sx={{ zIndex: 1 }}>
+                                  <ServerCard server={e} />
+                              </Grid>
+                          ))}
+                    {!isFetching && sortedServerList.length === 0 && (
+                        <Grid item xs={12} sx={{ minHeight: '300px' }}>
+                            <Page style={{ height: '100%' }}>
+                                <Typography variant="h6">No servers found</Typography>
+                            </Page>
+                        </Grid>
+                    )}
+                </Grid>
+
+                <Pagination
+                    variant="outlined"
+                    shape="rounded"
+                    size="large"
+                    count={Math.ceil((serverList?.totalItemCount ?? 0) / searchParams.perPage)}
+                    page={searchParams.page + 1}
+                    onChange={(_e, page) => setSearchParams({ ...searchParams, page: page - 1 })}
+                />
+            </Stack>
         </Page>
     );
 };
---
+++ src/pages/Home/TagFilterer.tsx
@@ -0,0 +1,101 @@
+import { Button, Container, Paper, Stack, Switch, Typography } from '@mui/material';
+import { FC, useCallback, useMemo, useState } from 'react';
+import { ServerTagSelector } from '../../components/ServerTagSelector/ServerTagSelector';
+import { ServerTagSearchOptions } from '../../types/Server/ServerTagSearchOptions';
+import { ServerTags } from '../../types/Server/ServerTags';
+
+export interface TagFiltererProps {
+    onApply: (searchInfo: ServerTagSearchOptions) => void;
+    onUnapply: () => void;
+}
+
+export const TagFilterer: FC<TagFiltererProps> = ({ onApply, onUnapply }) => {
+    const [filterTags, setFilterTags] = useState<ServerTags>(0);
+    const [tagJoinType, setTagJoinType] = useState<ServerTagSearchOptions['type']>('and');
+
+    const [applied, setApplied] = useState(false);
+    const [appliedWith, setAppliedWith] = useState<ServerTagSearchOptions>();
+
+    const dirty = useMemo(
+        () => appliedWith === undefined || appliedWith.tags !== filterTags || appliedWith.type !== tagJoinType,
+        [appliedWith, filterTags, tagJoinType],
+    );
+
+    const submittable = useMemo(() => filterTags > 0, [filterTags]);
+
+    const handleTagsChange = useCallback((newTags: ServerTags) => {
+        setFilterTags(newTags);
+    }, []);
+
+    const handleApply = useCallback(() => {
+        setApplied(true);
+        setAppliedWith({ tags: filterTags, type: tagJoinType });
+        onApply({ tags: filterTags, type: tagJoinType });
+    }, [filterTags, onApply, tagJoinType]);
+
+    const handleUnapply = useCallback(() => {
+        setApplied(false);
+        setAppliedWith(undefined);
+        setFilterTags(0);
+        setTagJoinType('and');
+        onUnapply();
+    }, [onUnapply]);
+
+    const handleReset = useCallback(() => {
+        setFilterTags(appliedWith?.tags ?? 0);
+        setTagJoinType(appliedWith?.type ?? 'and');
+    }, [appliedWith?.tags, appliedWith?.type]);
+
+    return (
+        <Container maxWidth="md">
+            <Paper elevation={2} square sx={{ p: 1 }}>
+                <Typography variant="h6" gutterBottom>
+                    Select tags to filter:
+                </Typography>
+                <ServerTagSelector tags={filterTags} onTagsChange={handleTagsChange} fromAll />
+
+                <Stack
+                    direction="row"
+                    gap={1}
+                    justifyContent="space-between"
+                    alignItems="center"
+                    sx={{ mt: 1 }}
+                    flexWrap="wrap"
+                >
+                    <Stack direction="row" alignItems="center">
+                        <Typography>Require any</Typography>
+                        <Switch
+                            checked={tagJoinType === 'and'}
+                            onChange={(e) => setTagJoinType(e.target.checked ? 'and' : 'or')}
+                        />
+                        <Typography>Require all</Typography>
+                    </Stack>
+
+                    <Stack direction="row" gap={1}>
+                        <Button
+                            disabled={!submittable || !dirty}
+                            onClick={handleApply}
+                            variant="outlined"
+                            color="success"
+                        >
+                            Apply
+                        </Button>
+
+                        <Button
+                            disabled={appliedWith === undefined || !dirty}
+                            onClick={handleReset}
+                            variant="outlined"
+                            color="warning"
+                        >
+                            Reset
+                        </Button>
+
+                        <Button disabled={!applied} onClick={handleUnapply} variant="outlined" color="error">
+                            Clear
+                        </Button>
+                    </Stack>
+                </Stack>
+            </Paper>
+        </Container>
+    );
+};
